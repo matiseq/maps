1032ccfc959cb6a4c879f846dd956bb7a5470119
matiseq <matiseq@int.pl>
2023-02-22 19:55:36 +0100

    map is added

--- src/component/map.js
+++ src/component/map.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import './map.css';
+import { MapContainer, TileLayer, useMap, Popup, Marker } from 'react-leaflet';
 
 function Map() {
-  return <h1>Map działa</h1>;
+  return (
+    <div>
+      <h1>Map działa</h1>
+      <div id="map">
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    </div>
+  );
 }
 
 export default Map;
