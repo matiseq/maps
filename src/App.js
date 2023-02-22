fdb3df5e541e491498911928701cba394ea0b15a
matiseq <matiseq@int.pl>
2023-02-22 16:42:43 +0100

    routing + mui install

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './component/home';
+import Map from './component/map';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ul className="App-header">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/map">Map</Link>
+          </li>
+        </ul>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/map" element={<Map />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/component/map.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Map() {
+  return <h1>Map działa</h1>;
+}
+
+export default Map;
