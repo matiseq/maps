b70ced24ad2da10e3878a41b9d40e34788c03384
matiseq <matiseq@int.pl>
2023-02-22 17:26:23 +0100

    home page inputs

--- src/component/home.js
+++ src/component/home.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
 function Home() {
-  return <h1>Home działa</h1>;
+  return (
+    <div>
+      <h1>
+        <code>Home</code>
+      </h1>
+      <form>
+        <label>
+          <code>Adres początkowy:</code>
+          <input type="text" name="Adres początkowy" />
+        </label>
+        <input type="submit" value="Zatwierdź" />
+      </form>
+      <form>
+        <label>
+          <code>Adres końcowy:</code>
+          <input type="text" name="Adres początkowy" />
+        </label>
+        <input type="submit" value="Zatwierdź" />
+      </form>
+    </div>
+  );
 }
 
 export default Home;
